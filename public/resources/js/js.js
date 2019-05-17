5122c1fb43f27c9d924cfe7a2a3d5c485a7d8a04
Rabbit <Corbineau@users.noreply.github.com>
2019-05-16 23:18:37 -0400

    nodeing it up

---
+++ public/resources/js/js.js
@@ -0,0 +1,105 @@
+require("dotenv").config();
+var fs = require("fs");
+var express = require("express");
+var path = require("path");
+
+
+
+
+//the poem object
+class Poem {
+    constructor(lineNum) {
+        this.name = "";
+        this.lineNum = lineNum;
+        this.myLines = [];
+        this.author = "";
+
+        //you will have the option to name the poem if you choose to save it
+        this.saveName = function(name) {
+            this.name = name;
+        }
+
+        //likewise, you can add an author name
+        this.saveAuthor = function(author) {
+            this.author = author;
+        }
+
+        //method to generate the poem itself
+        this.genPoem = function() {
+            fs.readFile("lines.txt", "utf8", function (error, data) {
+
+                if (error) {
+                    return console.log(error);
+                }
+        
+                let dataArr = data.split(",");
+                for (let i = 0; i <= this.lineNum; i++) {
+                    let index = Math.floor(Math.random() * dataArr.length);
+                    let oneLine = `${dataArr[index]}<br />`;
+                    console.log(oneLine);
+                    this.myLines.push(oneLine);
+                }
+                //does this need to be a promise?
+            }
+            )
+        }
+    }
+}
+
+storeLine = () => {
+    let line = $("#lineAdd").val().trim();
+    console.log(`The line: ${line}`);
+    //save it in a text file! HOWSABOUT DAT!?
+    fs.appendFile("lines.txt", `${line},`, function (err) {
+        if (err) throw err;
+
+    });
+    $("#lineAdd").val("");
+}
+
+
+$("#lineBtn").on("click", (event) => {
+    event.preventDefault();
+    storeLine();
+})
+
+//set changable html element
+
+let poemDiv = $("#poem");
+
+// Generate the poem
+
+const showPoem = () => {
+    let thisPoem = new Poem($("#lineNumber").val().trim());
+    thisPoem.genPoem();
+    poemDiv.text(thisPoem.myLines.join(" "));
+    $("#lineNumber").val("");
+}
+
+const 
+
+const savePoem = (poemName) => {
+db.collection("poems").doc(`${poemName}`).set({
+    name: poemName,
+    lines: myLines,
+    author: author
+})
+.then(function() {
+    console.log("Document successfully written!");
+})
+.catch(function(error) {
+    console.error("Error writing document: ", error);
+});
+
+}
+
+$("#genPoem").on("click", (event) => {
+    event.preventDefault();
+    genPoem();
+})
+
+let storedLines = localStorage.getItem(JSON.parse("lines"));
+    console.log(storedLines);
+if (storedLines) {
+    poemLines = JSON.parse(storedLines);
+};
