9dedd3918537e5a7dc329bd5027f1646c037adca
corbineau <lochinvar@gmail.com>
2019-10-10 22:50:13 -0400

    got start script to work anyway

--- js.js
+++ js.js
@@ -49,7 +49,7 @@ let thisPoem;
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../../index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
   });
   
   // Displays all poems
